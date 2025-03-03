e43379e925d24fd245ed9b4b86e81ae2a8d252d6
izartorius <atreuseinzbelt@gmail.com>
2025-03-03 15:39:29 +0800

    prints the bigger number out of 2 inputs

---
+++ 1.py
@@ -0,0 +1,9 @@
+num = float(input('enter a number: '))
+num2 = float(input('enter a number: '))
+
+if num > num2:
+    print(num)
+elif num2 > num:
+    print(num2)
+else:
+    print("both numbers are equal")

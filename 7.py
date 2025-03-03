03be02aba4c7f65c5cfb4f495856f9e07309fd5d
izartorius <atreuseinzbelt@gmail.com>
2025-03-03 16:28:15 +0800

    prints the sum of all 10 inputs

---
+++ 7.py
@@ -0,0 +1,10 @@
+total = 0
+a = float(input('enter the 1st number: '))
+b = float(input('enter the 2nd number: '))
+c = float(input('enter the 3rd number: '))
+total += a + b + c
+
+for i in range(4, 11):
+    num = float(input(f"enter the {i}th number : "))
+    total += num
+print("sum of numbers:", total)
